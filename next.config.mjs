7bbe853f6004c08749a8701d2ec1b547e7df763c
Akash Khatale <126044118+AKA2114SH@users.noreply.github.com>
2026-05-26 13:17:53 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  images: { unoptimized: true },
-  basePath: '/akash-portfolio-2026', 
+  images: {
+    unoptimized: true,
+  },
+  // This line fix: it only adds the sub-folder if deploying to GitHub
+  basePath: process.env.NODE_ENV === 'production' && !process.env.VERCEL ? '/v0-akash-portfolio.v0.app' : '',
 }
-export default nextConfig;
+
+export default nextConfig
