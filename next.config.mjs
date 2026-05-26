f15dcf4fe77149ac069c0d00b74ce86f20d5d4e0
Akash Khatale <126044118+AKA2114SH@users.noreply.github.com>
2026-05-26 12:56:27 +0530

    Simplify next.config.mjs configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  images: {
-    unoptimized: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  // Ensure the basePath matches your current repository name exactly
+  images: { unoptimized: true },
   basePath: '/akash-portfolio-2026', 
 }
-
-export default nextConfig
+export default nextConfig;
