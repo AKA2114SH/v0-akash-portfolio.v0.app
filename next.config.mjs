50f17f8898cd6456949860a4bfe34bef9a299aac
Akash Khatale <126044118+AKA2114SH@users.noreply.github.com>
2026-05-26 12:48:42 +0530

    Update next.config.mjs for output and basePath

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
+  output: 'export',
   images: {
     unoptimized: true,
   },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  // Ensure the basePath matches your current repository name exactly
+  basePath: '/akash-portfolio-2026', 
 }
 
 export default nextConfig
